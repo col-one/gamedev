4fe45d9c9e32de81721059d82e06f9c077eb880e
col-one <colin-one@hotmail.com>
2015-03-26 00:52:42 +0100

    add BaseWeaponController and classique prefab

--- SandBox/Assets/Scripts/FireTest.cs
+++ SandBox/Assets/Scripts/FireTest.cs
@@ -3,43 +3,18 @@ using System.Collections;
 
 public class FireTest : MonoBehaviour {
 
-	public BaseWeapon weapon;
-	
-	public enum ParentDir{X, Y, Z};
-	public ParentDir fireDirections;
+	public BaseWeaponController weapon;
 
-	private Vector3 localVelocity;
 
 	// Use this for initialization
 	void Start () {
-		weapon = GetComponent<BaseWeapon>();
-		weapon.Enable();
-		weapon.Reloaded();
+		weapon = (BaseWeaponController) GetComponent<BaseWeaponController>();
+		//weapon.SetWeaponSlot(0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		localVelocity = transform.InverseTransformDirection(weapon.projectileGo.rigidbody.velocity);
-
-		switch(fireDirections)
-		{
-		case ParentDir.X:
-			localVelocity.x = 1;
-			break;
-		case ParentDir.Y:
-			localVelocity.y = 1;
-			break;
-		case ParentDir.Z:
-			localVelocity.z = 1;
-			break;
-		default:
-			localVelocity = new Vector3(0,0,0);
-			break;
-		}
-
-		localVelocity = transform.TransformDirection(localVelocity);
-
-		weapon.Fire(localVelocity);
+		weapon.Fire();
 	}
 }
